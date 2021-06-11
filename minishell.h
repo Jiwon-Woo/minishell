49c74f7304823473d905ae01fbaaba77ef270d5e
Chanyeop Lim <clim@c5r3s6.42seoul.kr>
2021-06-11 22:16:26 +0900

    20210611

--- minishell.h
+++ minishell.h
@@ -1,33 +1,34 @@
 #ifndef MINISHELL_H
-# define MINI_SHELL_H
+# define MINISHELL_H
 
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
+# include <string.h>
 
 typedef struct	s_history
 {
 	char		*line;	
-	t_history	*next;
+	struct s_history	*next;
 }				t_history;
 
 typedef struct	s_env
 {
 	char		*name;
 	char		*value;
-	t_env		*next;
+	struct s_env		*next;
 }				t_env;
 
 typedef struct	s_echo
 {
 	char		*str;
-}
+}				t_echo;
 
 typedef struct	s_pwd
 {
 	char		*cur_dir;
-}
+}				t_pwd;
 
 #endif
 
