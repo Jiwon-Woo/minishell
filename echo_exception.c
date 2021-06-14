9b8e08bc729677fd87246b27ef20b10a7a9259aa
Jiwon Woo <jwoo@c5r3s1.42seoul.kr>
2021-06-14 22:22:55 +0900

    2021/6/14

--- echo_exception.c
+++ echo_exception.c
@@ -7,6 +7,10 @@
 // 		return (1);
 // 	return (0);
 // }
+// 음    음,, fork 해서 exec 까지 해볼게요 그럼
+// 일단 미니쉘이 바쉬기준이니까 ./minishell 하자마자 bash를 켜야할까요
+// 좋아요 어떻게 하는 건진 모르겠지만...
+// 아님 할줄 아시면 한번 보여주세요~! // 오 감사합니다 포커싱해놓을게요
 
 int 		get_arg_num(char *line)
 {
@@ -17,7 +21,7 @@ int 		get_arg_num(char *line)
 	arg_num = 1;
 	while(*line)
 	{
-		if (*line == '\"' &&|| *line == '\'')
+		if (*line == '\"' && *line == '\'')
 		{
 			line++;
 			if (*line == '\"')
